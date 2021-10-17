9f2545d00cb50c5efddc89374f3b32d154105978
khk <jasun1208@naver.com>
2021-10-17 11:07:49 +0900

    Ajax 통신 수정

--- crime_detection/upload/views.py
+++ crime_detection/upload/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import render
 import os
 # from django.core.files.storage import FileSystemStorage
 import json
+
+from django.utils.html import format_html
 from . import forms
 from django.views.decorators.csrf import csrf_exempt
 
 from crime_project import settings
 
 from PIL import Image
+from django.utils.safestring import SafeData, SafeString, mark_safe
+import base64
 
 # def post_view(request):
 #     return render(request, 'upload/post.html')
@@ -17,29 +21,42 @@ from PIL import Image
 def result(request):
 
     form = forms.UploadForm(request.POST, request.FILES)
+    # print(form)
     if form.is_valid():
-        clean_data = form.cleaned_data
-        img_field = clean_data['upimg']
+        # clean_data = form.cleaned_data
+        # # print(clean_data.read)
+        # img_field = clean_data['upimg']
+        # # print(img_field.read())
 
-        print(img_field, type(img_field))
-        print(img_field.image.width, img_field.image.height, img_field.image.format, img_field.name)
+        # print(img_field, type(img_field))
+        # # print(img_field.image.width, img_field.image.height, img_field.image.format, img_field.name)
 
-        image = Image.open(img_field)
-        print(type(image))
+        # image = Image.open(img_field)
+        # print(type(image))
 
         # 파일저장
-        save_path = os.path.join(settings.MEDIA_ROOT, img_field.name)
-        print(save_path)
-        image.save(save_path)
-        print(r"/media/{}".format(img_field.name))
-        # dictionary -> JSON 변환시 numpy 타입은 변환이 안된다. str(), float()으로 타입변환
-        result = {
-            'img_url': r"/media/{}".format(img_field.name)
-        }
+        # save_path = os.path.join(settings.MEDIA_ROOT, img_field.name)
+        # print(save_path)
+        # image.save(save_path)
+        # print(r"/media/{}".format(img_field.name))
+        # # dictionary -> JSON 변환시 numpy 타입은 변환이 안된다. str(), float()으로 타입변환
+        # result = {
+        #     'img_url': r"/media/{}".format(img_field.name)
+        # }
+        # result_str = json.dumps(result)
 
-        result_str = json.dumps(result)
 
-        return HttpResponse(result_str)
+        
+        in_file = request.FILES['upimg']
+        bytes_file = in_file.read()
+        # print(type(bytes_file))
+        encode_data = base64.b64encode(bytes_file).decode('utf-8')
+        print(type(encode_data))
+
+   
+        result = {'image_data':encode_data}
+        result = json.dumps(result)
+        return HttpResponse(result)
 
 
     # if request.method =='GET':
